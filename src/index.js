c5c5292e6ada7729515c77d4377ccd3af90d8567
Frank van der Hoek <frank.vanderhoek@gmail.com>
2016-11-02 17:45:38 +0100

    Fix caching in componentWillMount

--- src/index.js
+++ src/index.js
@@ -8,49 +8,63 @@ const connect = (mapStorageToProps) => {
 			state = {};
 
 			componentWillMount() {
-				this.obtainDownloadURLs(this.props, {cacheOnly: true});
+				this.obtainDownloadURLsFromCache(this.props);
 			}
 
 			componentDidMount() {
-				this.obtainDownloadURLs(this.props, {firebaseOnly: true});
+				this.updateCacheWithDownloadURLs(this.props);
 			}
 
 			componentWillReceiveProps(next) {
-				this.obtainDownloadURLs(next);
+				this.obtainDownloadURLsFromCache(next);
+				this.updateCacheWithDownloadURLs(next);
 			}
 
-			obtainDownloadURLs = (props, options = {}) => {
-				const {cacheOnly, firebaseOnly} = options;
+			obtainDownloadURLsFromCache(props) {
+				const mapping = mapStorageToProps(props);
+				const keys = Object.keys(mapping);
+				const newState = {}; // Holds the updated state
+
+				for (let i = keys.length - 1; i >= 0; --i) {
+
+					// Only check keys that have a mapping value
+					if (!mapping[keys[i]]) {
+						continue;
+					}
+
+					// Check cache based on full path
+					const url = cache[mapping[keys[i]].fullPath];
+					if (url) {
+						// Add url to new state
+						newState[keys[i]] = url;
+					}
+				}
+				this.setState(newState);
+			}
+
+			updateCacheWithDownloadURLs(props) {
 				const mapping = mapStorageToProps(props);
 				const keys = Object.keys(mapping);
 				const newState = {}; // Holds the updated state
 
 				Promise.all(keys.map(prop => {
-					// Only obtain URL if mapping is defined
+
+					// Only check keys that have a mapping value
 					if (!mapping[prop]) {
-						return null;
+						return;
 					}
-					const fullPath = mapping[prop].fullPath;
 
-					if (!firebaseOnly) {
-						const url = cache[fullPath];
-						if (url) {
-							// Add url to new state
+					// Ignore if already in cache
+					if(cache[mapping[prop].fullPath]) {
+						return;
+					}
+
+					return mapping[prop].getDownloadURL()
+						.then((url) => {
+							// Save url in cache and new state
+							cache[mapping[prop].fullPath] = url;
 							newState[prop] = url;
-							return url;
-						}
-					}
-
-					if (!cacheOnly) {
-						return mapping[prop].getDownloadURL()
-							.then((url) => {
-								// Save url in cache and new state
-								newState[prop] = cache[fullPath] = url;
-								return url;
-							});
-					}
-
-					return null;
+						});
 				}))
 					.then(() => this.setState(newState))
 					.catch(console.error);
